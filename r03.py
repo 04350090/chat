d49a111a12d1a57014747297be00bc0dfe1a9160
Alan Tai <alan5710804@gmail.com>
2020-10-31 00:50:51 +0800

    r03.py

---
+++ r03.py
@@ -0,0 +1,13 @@
+
+lines = []
+with open('3.txt', 'r', encoding="utf-8-sig") as f:
+	for line in f:
+		lines.append(line.strip())
+
+for line in lines:
+	s = line.split(" ")
+	time = s[0][:5]
+	name = s[0][5:]
+	print("時間：", time, ",名字：", name)
+	# print(line)
+# 清單的切割也可以使用在字串上
